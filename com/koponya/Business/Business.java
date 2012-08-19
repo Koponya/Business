34a82b0091077f6a758eb828f229cb0ddc88c4a8
Koponya <gaga1004@gmail.com>
2012-08-19 18:29:39 +0200

    initial commit

---
+++ com/koponya/Business/Business.java
@@ -0,0 +1,18 @@
+package com.koponya.Business;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Business extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+		
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+
+}
